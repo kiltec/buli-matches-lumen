afed2314b78163ea431d1543e03c95c6ccaee72b
Lars Tesmer <lars.tesmer@gmail.com>
2017-09-10 15:51:37 +0200

    Season overview can now be visited without mocking
    For example, in the browser... :D
    Woah, that cost me to figure out that I had to
    uncomment the line in boostrap/app.php... <_<

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use App\Http\Controllers\MatchListingController;
+use App\OpenLiga\Clients\HttpClient;
+use App\OpenLiga\SeasonService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +15,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(HttpClient::class, function ($app) {
+            return new HttpClient();
+        });
+
+        $this->app->bind(SeasonService::class, function ($app) {
+            return new SeasonService($app->make(HttpClient::class));
+        });
+
+        $this->app->bind(MatchListingController::class, function ($app) {
+            return new MatchListingController($app->make(SeasonService::class));
+        });
+
     }
 }
