925056bb077daf8f1d0117296e5f2aaaea1a8ecc
Lars Tesmer <lars.tesmer@gmail.com>
2017-09-11 01:01:03 +0200

    Fix wording on homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
                     <div class="jumbotron">
                         <h1 class="display-3">Bundesliga Matches</h1>
                         <p class="lead">
-                            A small project to explore Lumen and the <a href="https://www.openligadb.de/">OpenLigaDB.de
+                            A small project to explore Lumen, the <a href="https://www.openligadb.de/">OpenLigaDB.de
                                 API</a> and to hone my skills.
                         </p>
                         <hr class="my-4">
